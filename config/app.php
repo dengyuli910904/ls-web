50312cda169b3201e83cf9a1c5f6f3a9e626ba11
livesong <39078525@qq.com>
2017-06-14 13:11:39 +0800

    'add sendcloud config & guzzle config'

--- config/app.php
+++ config/app.php
@@ -64,7 +64,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'PRC',
 
     /*
     |--------------------------------------------------------------------------
@@ -176,6 +176,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        Naux\Mail\SendCloudServiceProvider::class,//sendcloud发送邮件
 
     ],
 
