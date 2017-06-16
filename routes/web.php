417f18a124b0f28d38f0c3282a403bb4cef25dfd
livesong <39078525@qq.com>
2017-06-16 19:03:11 +0800

    add users

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('email/verify/{token}',['as' => 'email.verify', 'users' => 'EmailController@verify']);
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
