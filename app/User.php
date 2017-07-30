9c8377440cc34d7d362a3f3217a82fdd0b241eac
livesong <39078525@qq.com>
2017-07-30 21:20:23 +0800

    mv User.php

---
+++ app/User.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+use Mail;
+use Naux\Mail\SendCloudTemplate;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password', 'avatar', 'confirmation_token'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * 重写发送密码重置邮件
+     *
+     * @param string $token
+     */
+    public function sendPasswordResetNotification($token)
+    {
+        $data = [ 'url' => url('password/reset', $token)];
+
+        $template = new SendCloudTemplate('password_reset_template', $data);
+
+        Mail::raw($template, function ($message) {
+
+            $message->from('customers@livesong.cn', 'Laravel');
+            $message->to($this->email);
+        });
+    }
+}
